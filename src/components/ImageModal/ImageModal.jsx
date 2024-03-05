ee33b33b062cdacf32fb0f3a9c684e33e6c9cd26
ValeriiaProkhina <applehouse1883@icloud.com>
2024-03-05 22:51:19 +0200

    fix ImageModal.jsx

--- src/components/ImageModal/ImageModal.jsx
+++ src/components/ImageModal/ImageModal.jsx
@@ -5,16 +5,24 @@ ReactModal.setAppElement("#root");
 
 export const ImageModal = ({ isOpen, onClose, content }) => {
   return (
-    <ReactModal
-      isOpen={isOpen}
-      onRequestClose={onClose}
-      shouldCloseOnOverlayClick={true}
-      shouldCloseOnEsc={true}
-      className={css.modal}
-      overlayClassName={css.overlay}
-    >
-      <img src={content.urls.regular} className={css.img} />
-      {content.description && <p className={css.text}>{content.description}</p>}
-    </ReactModal>
+    content.urls.regular && (
+      <ReactModal
+        isOpen={isOpen}
+        onRequestClose={onClose}
+        shouldCloseOnOverlayClick={true}
+        shouldCloseOnEsc={true}
+        className={css.modal}
+        overlayClassName={css.overlay}
+      >
+        <img
+          src={content.urls.regular}
+          alt={content.alt_description}
+          className={css.img}
+        />
+        {content.description && (
+          <p className={css.text}>{content.description}</p>
+        )}
+      </ReactModal>
+    )
   );
 };
